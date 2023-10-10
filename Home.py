8917a4f6c54c65db07446e66b3b4543192775bec
Jadson <125108476+JadsonDS@users.noreply.github.com>
2023-10-10 17:13:30 -0300

    Add files via upload

--- Home.py
+++ Home.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 st.set_page_config(
     page_title = 'Home',
-    page_icon = ':house:',
-    layout='wide')
+    page_icon = ':house:')
+    
 
 image=Image.open('logo.png')
 st.sidebar.image(image, width=340)
@@ -20,20 +20,20 @@ st.markdown(
     Growth Dashbord foi construído para acompanhar as métricas de crescimento dos Entregadores, Restaurantes e Empresas.
     ### Como Utilizar esse Grouwth Dashbord?:
     
-    - Visão Empresa:
+    - #### Visão Empresa:
         - Visão Geremcial: Métrica gereais de comportamento.
         - Visão Tática: Indicadores semanais de crescimento.
         - Visão Geográfica : Insights de geolocalização.
         
         
-    - Visão Entregador:
+    - #### Visão Entregador:
       - Acompanhamento dos indicadores semanais de crescimento.
       
         
-    - Visão Restaurantes: 
+    - #### Visão Restaurantes: 
         - Indicadores semanais de crescimento dos restaurantes
         
-    ### Ask for Help
+    #### Ask for Help
     - Time de Data Science no  Discord    
         - @jadson            
 """)
